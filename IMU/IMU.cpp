34e3461e3b153039c8feab72b640838166f34bc6
bkondrat <kb.pcre@gmail.com>
2015-01-27 00:02:23 +0100

    init

---
+++ IMU/IMU.cpp
@@ -0,0 +1,31 @@
+// Do not remove the include below
+#include "IMU.h"
+#include <ITG3200.h>
+#include <Arduino.h>
+#include <Wire.h>
+
+//The setup function is called once at startup of the sketch
+
+ITG3200 gyro;
+int x;
+int y;
+int z;
+void setup() {
+	Serial.begin(115200);
+	gyro = ITG3200();
+	gyro.init(ITG3200_ADDR_AD0_HIGH);
+	Serial.println("Calibrating Gyro...");
+	gyro.zeroCalibrate(1000, 2);
+}
+
+// The loop function is called in an endless loop
+void loop() {
+	delay(1000);
+	gyro.readGyroRawCal(&x, &y, &z);
+	Serial.print(x);
+	Serial.print(" ");
+	Serial.print(y);
+	Serial.print(" ");
+	Serial.print(z);
+	Serial.print("\n");
+}
